6f106caba7a8cf3c79659d2a34155314fc3ba3e5
ContDiscDev1 <jmaldonado@contdisc.com>
2022-08-10 22:40:33 -0500

    Added index html template

--- gasprices/gasprices/settings.py
+++ gasprices/gasprices/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'gasprices.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['gasprices/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- gasprices/gasprices/views.py
+++ gasprices/gasprices/views.py
@@ -1,6 +1,14 @@
+from email import contentmanager
+from multiprocessing import context
+from re import template
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the Gas Prices Web App")
+    template = loader.get_template('gasprices/index.html')
+    context = {'something':'here is something'}
+    return HttpResponse(template.render(context))
