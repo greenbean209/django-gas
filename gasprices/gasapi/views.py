82eb0d9639e95a1870e9ec0e7820c9bf90ff0324
ContDiscDev1 <jmaldonado@contdisc.com>
2022-08-10 21:19:57 -0500

    added houston gas prices to gasapi

--- gasprices/gasapi/views.py
+++ gasprices/gasapi/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .gasprices_reader import get_gasprices_html
+
 # Create your views here.
 def index(request):
     return HttpResponse("Gas Prices API")
+
+def houston(request):
+    response = get_gasprices_html()
+    return HttpResponse(response, content_type="text/HTML")
